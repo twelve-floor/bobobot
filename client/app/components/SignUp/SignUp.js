1b6b7bc84d55306a401f8db96d83633f84e74b92
sergey <sergey@powr.io>
2019-06-25 22:25:46 +0600

    Implement signup or signin on client side

--- client/app/components/SignUp/SignUp.js
+++ client/app/components/SignUp/SignUp.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import axios from 'axios';
 
 function MadeWithLove() {
   return (
@@ -49,43 +48,116 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export default function SignUp() {
+export default function SignUp(props) {
   const classes = useStyles();
+  const token = localStorage.getItem('token');
+  if (token != null) {
+    props.history.push('/calendar');
+  }
+  const [isSignUp, setIsSignUp] = useState(true);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSuccess(res) {
+    localStorage.setItem('token', res.data.token);
+    props.history.push('/calendar');
+  }
+
+  function handleError(error) {
+    // console.log(error);
+    alert(error);
+  }
+
+  function signUp() {
+    if (!name && !email && !password) {
+      return;
+    }
+    const userData = {
+      name,
+      email,
+      password,
+    };
+    axios
+      .post('/api/users/register', userData)
+      .then(handleSuccess)
+      .catch(handleError);
+  }
+
+  function signIn() {
+    if (!email && !password) {
+      return;
+    }
+    const userData = {
+      email,
+      password,
+    };
+    axios
+      .post('/api/users/login', userData)
+      .then(handleSuccess)
+      .catch(handleError);
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+
+    if (isSignUp) {
+      signUp();
+    } else {
+      signIn();
+    }
+  }
+
+  function onUpdateName(e) {
+    setName(e.target.value);
+  }
+
+  function onUpdateEmail(e) {
+    setEmail(e.target.value);
+  }
+
+  function onUpdatePassword(e) {
+    setPassword(e.target.value);
+  }
+
+  const nameInput = isSignUp && (
+    <Grid item xs={12}>
+      <TextField
+        autoComplete="fname"
+        name="name"
+        variant="outlined"
+        required
+        fullWidth
+        id="name"
+        label="Name"
+        autoFocus
+        data-lpignore="true"
+        value={name}
+        onChange={onUpdateName}
+      />
+    </Grid>
+  );
+
+  function switchSignIn() {
+    setIsSignUp(!isSignUp);
+  }
+
+  const signUpButtonName = isSignUp ? 'Sign Up' : 'Sign In';
+  const switchButtonName = isSignUp
+    ? 'Already have an account? Sign in'
+    : 'Registration';
 
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-        </Avatar>
+        <Avatar className={classes.avatar} />
         <Typography component="h1" variant="h5">
-          Sign up
+          {isSignUp ? 'Sign up' : 'Sign in'}
         </Typography>
         <form className={classes.form} noValidate>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="fname"
-                name="firstName"
-                variant="outlined"
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
-              />
-            </Grid>
+            {nameInput}
             <Grid item xs={12}>
               <TextField
                 variant="outlined"
@@ -94,7 +166,9 @@ export default function SignUp() {
                 id="email"
                 label="Email Address"
                 name="email"
-                autoComplete="email"
+                data-lpignore="true"
+                value={email}
+                onChange={onUpdateEmail}
               />
             </Grid>
             <Grid item xs={12}>
@@ -107,12 +181,9 @@ export default function SignUp() {
                 type="password"
                 id="password"
                 autoComplete="current-password"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration, marketing promotions and updates via email."
+                data-lpignore="true"
+                password={password}
+                onChange={onUpdatePassword}
               />
             </Grid>
           </Grid>
@@ -122,14 +193,13 @@ export default function SignUp() {
             variant="contained"
             color="primary"
             className={classes.submit}
+            onClick={onSubmit}
           >
-            Sign Up
+            {signUpButtonName}
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
-                Already have an account? Sign in
-              </Link>
+              <Button onClick={switchSignIn}>{switchButtonName}</Button>
             </Grid>
           </Grid>
         </form>
