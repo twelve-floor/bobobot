50f2cb255f5a9417b153c04bf0bdf6de53517b4e
sergey <sergey@powr.io>
2019-06-29 17:01:51 +0600

    remove wrong dependency

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -3,7 +3,6 @@ const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const helpers = require('./helpers');
 
